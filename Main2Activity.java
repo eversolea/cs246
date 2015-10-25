855100b293b360bd2ec08bbeb4ef065bb68a1483
eversolea <eve12004@byui.edu>
2015-10-25 00:19:12 -0600

    Create Main2Activity.java

---
+++ Main2Activity.java
@@ -0,0 +1,34 @@
+package test.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Globals g = Globals.getInstance();
+        TextView view = (TextView) findViewById(R.id.scriptures);
+        view.setText("Your favorite scripture is: " + g.book + " " + g.chapter + ":" + g.verse);
+    }
+
+}
